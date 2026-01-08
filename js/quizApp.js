63c3e74423fe1ec2c8030ca5db44fa578c7784df
Adekogbe Gideon <adekogbe08@gmail.com>
2026-01-08 12:00:33 +0100

    Qestions script

---
+++ js/quizApp.js
@@ -0,0 +1,37 @@
+const quizInit = function () {
+  const questions = [
+    {
+      questions: "Who founded Facebook?",
+      options: ["Mark Zuckerberg", "Elon Musk", "Tim Cook", "Pavel Durov"],
+      answer: "Mark Zuckerberg",
+    },
+
+    {
+      questions: "Who discoverd gravity?",
+      options: ["Aristotle", "Isaac Newton", "Pythagoras", "Albert Einstine"],
+      answer: "Isaac Newton",
+    },
+
+    {
+      questions: "Who is the founder of PXXL Space?",
+      options: [
+        "Carls Robinson",
+        "Micheal Ederson",
+        "Robinson Honour",
+        "Isreal Carnot",
+      ],
+      answer: "Robinson Honour",
+    },
+    {
+      questions: "What is the capital city of Canada?",
+      options: ["Vancouver", "Calgary", "Ottawa", "Toronto"],
+      answer: "Ottawa",
+    },
+
+    {
+      questions: "Who discoverd gravity?",
+      options: ["Aristotle", "Isaac Newton", "Pythagoras", "Albert Einstine"],
+      answer: "Isaac Newton",
+    },
+  ];
+};
